5f0087daf5b698e8df2056025e803106e371c05e
Scott <clearplaid@gmail.com>
2019-04-10 11:44:42 -0400

    added files and pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+
+// If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+// The scenario is similar for wrong answers and time-outs.
+
+// If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+// If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+// On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
+//create variables
+//create start button that begins trivia once clicked
+//main page should have instructions displayed until start button clicked
+//on click function(startGame) loop through all the questions.length display question and possible answers(should be clickable buttons to select)
+//set timer for x seconds to answer question
+//if clicked correct answer before time runs out show victory screen for y seconds and display next question and increment answersCorrect++
+//else if clicked wrong answer tell user they are wrong and display correct answer wait y seconds and display next question and increment answersWrong++
+//else  show timed out screen and display correct answer wait y seconds and display next question and increment answersWrong++
+//after looped through all questions display final screen with correct answers, wrong answers, and a restart game button that brings you back to the beginning of the game
