680712962d29487ce3cda6f9c43b2d9954f9ba1c
Scott <clearplaid@gmail.com>
2019-04-13 15:08:13 -0400

    added game start function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
     // hide timer
     $("#time-remaining").hide();
     // start game
-    $("#start").on("click", triviaGame.Start);
+    $(".start").on("click", triviaGame.gameStart);
     // check users choices
     $(document).on("click", ".choices", triviaGame.choicesCheck);
 });
@@ -22,45 +22,76 @@ $(document).ready(function() {
         correctAnswer: 0,
         wrongAnswer: 0,
         unanswered: 0,
-        setCounter = 0,
         timer: 20,
         
-        questions = {
+        currentQuestion: 0,
+        qaBundle: [{question: "What year did Nintendo start making video games?",
+                     choices: ["1973", "1987", "2010", "1983"],
+                     answer: 0},
+                    {question: "In what game did Mario make his first appearance?",
+                     choices: ["Super Mario Bros.", "Donkey Kong", "Duck Hunt", "Mario Kart 64"],
+                     answer: 1},
+                    {question: "What was Mario's name before his name was Mario?",
+                     choices: ["Punchman", "Plumber Man", "Jumpman", "Carpenter Man"],
+                     answer: 2},
+                    {question: "What did Nintendo do before video games?",
+                     choices: ["Playing Cards", "Love Hotels", "Baseball", "Watches"],
+                     answer: 0},
+                    {question: "What is the name of the enemy that kidnaps Princess Peach?",
+                     choices: ["Kamek", "Lakitu", "Yoshi", "Bowser"],
+                     answer: 3}],
+
+        questions: {
             q1: "What year did Nintendo start making video games?",
             q2: "In what game did Mario make his first appearance?",
             q3: "What was Mario's name before his name was Mario?",
             q4: "What did Nintendo do before video games?",
             q5: "What is the name of the enemy that kidnaps Princess Peach?",
         },
-        choices = {
+        choices: {
             c1: ["1975", "1987", "2010", "1983"],
             c2: ["Super Mario Bros.", "Donkey Kong", "Duck Hunt", "Mario Kart 64"],
             c3: ["Punchman", "Plumber Man", "Jumpman", "Carpenter Man"],
             c4: ["Playing Cards", "Love Hotels", "Baseball", "Watches"],
             c5: ["Kamek", "Lakitu", "Bowser", "Yoshi"],
         },
-        answers = {
-            a1: "1983",
+        answers: {
+            a1: "1973",
             a2: "Donkey Kong",
             a3: "Jumpman",
             a4: "Playing Cards",
             a5: "Bowser",
         },
-
+    
     // methods for the trivia game
 
     // this method starts the game
     gameStart: function(){
         // hide start button
+        $('.start').hide();
         // display question and choices
-        // display and start timer; show time-remaining div
-        // check user choices if correct or not
-       
+        var question1 = triviaGame.qaBundle[0].question;
+        $('#questions').text(question1);
 
+        var choices = triviaGame.qaBundle[0].choices;
+
+        for (let i=0; i<choices.length; i++){
+            var choiceButton = $('<button>').text(triviaGame.qaBundle[0].choices[i]); 
+            choiceButton.addClass('choices');
+            $('#choices').append(choiceButton);
+        }
+        // display and start timer; show time-remaining div
+        $('time-remaining').show();
+        // check user choices if correct or not
+        
+      
     },
 
     choicesCheck: function(){
         // if choice equals answer increment correctAnswer
+        if (choiceButton === triviaGame.qaBundle[0].answer){
+            $('#results').text("Woo-hoo!");
+        }
         // display winning image for 5-10 seconds in results div
         // move to next question
         // else if choice !== correctAnswer increment wrongAnswer
@@ -96,7 +127,9 @@ $(document).ready(function() {
         // setCounter = 0,
         // timer: 20,
     },
+    
 }
+console.log(triviaGame)
 
 
 
